973291c4a5a3f65fb1fb00fe116ff91b09b15f30
Anna Kudas <nnkudas@gmail.com>
2023-08-03 11:30:30 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+
+const ulElement = document.getElementById("ingredients");
+
+
+const listItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  return listItem;
+});
+
+
+ulElement.append(...listItems);
