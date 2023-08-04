7f5569192739c224ee6b7f4dd1e68247acc88908
Anna Kudas <nnkudas@gmail.com>
2023-08-04 16:59:48 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const formData = new FormData(loginForm);
+  const formDataObject = {};
+
+  formData.forEach((value, name) => {
+    formDataObject[name] = value;
+  });
+
+  if (formDataObject.email.trim() === "" || formDataObject.password.trim() === "") {
+    alert("Please fill in all fields.");
+  } else {
+    console.log(formDataObject); 
+    loginForm.reset(); 
+});
+
+
+
+
+
 
