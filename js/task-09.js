1aa5a5acf950542b4041f252ac715ebf6dbede15
Anna Kudas <nnkudas@gmail.com>
2023-08-04 18:00:36 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,12 @@
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+changeColorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
