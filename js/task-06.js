18bdd3782b8ecd31266bdb394f85a39ee5505083
Anna Kudas <nnkudas@gmail.com>
2023-08-04 15:01:12 +0200

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,12 @@ input.addEventListener("blur", () => {
     const currentLength = input.value.length;
 
     if( currentLength === parseInt(inputLength)){
-        input.classList.remove("valid");
+        input.classList.remove("invalid");
         input.classList.add("valid");
     } else{
         input.classList.remove("valid");
         input.classList.add("invalid");
     }
-} )
+} );
+
+
