71641a93d54d7610c7c2140b9d6d6adeeb1216e1
Anna Kudas <nnkudas@gmail.com>
2023-08-05 11:31:37 +0200

    task 1 corrected

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const categoriesAmount = document.getElementById('categories');
 const items = categoriesAmount.querySelectorAll('li.item');
 
-console.log(`Total categories: ${items.length}`);
+console.log(`Number of categories: ${items.length}`);
 
 items.forEach((item) => {
   const categoryTitle = item.querySelector('h2').textContent;
   const subCategories = item.querySelectorAll('ul li');
-  console.log(`Category: ${categoryTitle}, Number of subcategories: ${subCategories.length}`);
+  console.log(`Category: ${categoryTitle}, Elements: ${subCategories.length}`);
 });
 
